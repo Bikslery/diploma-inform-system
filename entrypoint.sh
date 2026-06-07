ae96d5b052551c7d4ea7b385dc07491a446ee126
Bikslery <Bikslery@gmail.com>
2026-06-08 01:51:37 +0200

    Fix: store DB in persistent volume, auto-create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ mkdir -p /app/data
 
 if [ ! -f /app/data/db.sqlite3 ]; then
     python manage.py migrate --noinput
+    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin','admin@company.com','admin123')"
 fi
 
 python manage.py collectstatic --noinput
